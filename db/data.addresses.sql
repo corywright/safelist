a5e585b91ea36c36ee0df573ea8e98b011fbb300
Brett Elmendorf <brett@r26d.com>
2005-09-06 19:53:12 +0000

    Added Unknown Facility

--- db/data.addresses.sql
+++ db/data.addresses.sql
@@ -1,3 +1,4 @@
+insert into addresses (street,city,state,country) values('unknown','unknown','','USA');
 insert into addresses (street,city,state,country) values('Walzem','San Antonio','TX','USA');
 insert into addresses (street,city,state,country) values('Levi St','San Antonio','TX','USA');
 insert into addresses (street,city,state,country) values('Kelly AFB','San Antonio','TX','USA');
--- db/data.shelters.sql
+++ db/data.shelters.sql
@@ -1,3 +1,4 @@
-insert into shelters (name,address_id) values('Windsor World Shelter',1);
-insert into shelters (name,address_id) values('Levi Strauss',2);
-insert into shelters (name,address_id) values('Kelly USA',3);
+insert into shelters (name,address_id) values('Unknown',1);
+insert into shelters (name,address_id) values('Windsor World Shelter',2);
+insert into shelters (name,address_id) values('Levi Strauss',3);
+insert into shelters (name,address_id) values('Kelly USA',4);

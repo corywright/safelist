82a1bbcdd5ce28d302fe1925f02d4f7e3bbbc1a0
Brandon Williams <driftx@gmail.com>
2005-09-06 21:05:43 +0000

    Added form validation.

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,11 @@
 class Volunteer < ActiveRecord::Base
+  protected
+    def validate
+      errors.add_on_empty %w( first_name last_name )
+      errors.add("home_phone", "has invalid format") unless home_phone =~ /[0-9]*/
+    end
+
+    def validate_on_update
+      errors.add_to_base("No changes have occurred") if unchanged_attributes?
+    end
 end

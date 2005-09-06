c3f9cb45d8fe63fd0fae88ab03ce1c582a8094c8
Cory Wright <corywright@gmail.com>
2005-09-06 22:14:01 +0000

    Searching for people via tag_id and name is now implemented: /people/search

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,12 +9,42 @@ class PeopleController < ApplicationController
   end
 
   def show
-    @person = Person.find(1)
-	@pre_disaster_address = Address.find(@person.family.pre_disaster_address_id)
-	@shelter = Shelter.find(@person.shelter_id)
+    @person = Person.find(params[:id])
+    @pre_disaster_address = Address.find(@person.family.pre_disaster_address_id)
+    @shelter = Shelter.find(@person.shelter_id)
   end
   
   def edit
     @person = Person.find(params[:id])
+    @pre_disaster_address = Address.find(@person.family.pre_disaster_address_id)
+    @shelter = Shelter.find(@person.shelter_id)
   end
+
+  def search
+  end
+
+  def search_tag_id
+    @person = Person.find_by_tag_id(params[:tag_id])
+    if @person.nil?
+      flash[:notice] = "No one found by that tag id"
+      redirect_to :action => "search"
+    else
+      @pre_disaster_address = Address.find(@person.family.pre_disaster_address_id)
+      @shelter = Shelter.find(@person.shelter_id)
+      render :action => 'show'
+    end
+  end
+
+  def search_name
+    @person = Person.find_by_first_name_and_last_name(params[:first_name],params[:last_name])
+    if @person.nil?
+      flash[:notice] = "No one found by that name"
+      redirect_to :action => 'search'
+    else
+      @pre_disaster_address = Address.find(@person.family.pre_disaster_address_id)
+      @shelter = Shelter.find(@person.shelter_id)
+      render :action => 'show'
+    end
+  end
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,8 +2,9 @@ class Person < ActiveRecord::Base
     belongs_to :family
     belongs_to :person_type
     belongs_to :shelter
-	
-	def name
-		first_name + " " + last_name
-	end
+
+    def name
+      first_name + " " + last_name
+    end
+
 end

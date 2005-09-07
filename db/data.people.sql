a25023296f349a2d5547d3a808b9958b84f9c24e
Will Reese <wreese@gmail.com>
2005-09-07 20:25:02 +0000

    Added birthdates to the people test data.

--- db/data.people.sql
+++ db/data.people.sql
@@ -1,6 +1,6 @@
-INSERT INTO people (tag_id,first_name,last_name,family_id,person_type_id,medical_problems,ssn,shelter_id,location_description,resume)
-VALUES ('12342', 'Will', 'Reese', 1, 1, 'Lots', '123-12-1234', 1, 'Here','DBA, Ruby, Rails');
-INSERT INTO people (tag_id,first_name,last_name,family_id,person_type_id,medical_problems,ssn,shelter_id,location_description,resume)
-VALUES ('12343', 'gene', 'reese', 1, 2, 'Lots again', '123-12-1234', 2, '??????','Linux, Windows');
-INSERT INTO people (tag_id,first_name,last_name,family_id,person_type_id,medical_problems,ssn,shelter_id,location_description,resume)
-VALUES ('12345', 'Cory', 'Wright', 1, 3, 'Lots more', '123-12-1234', 3, 'He does not know','DNS, Subversion, PHP, Rails, Ruby');
+INSERT INTO people (tag_id,first_name,last_name,dob,family_id,person_type_id,medical_problems,ssn,shelter_id,location_description,resume)
+VALUES ('12342', 'Will', 'Reese', '1997-01-01', 1, 1, 'Lots', '123-12-1234', 1, 'Here','DBA, Ruby, Rails');
+INSERT INTO people (tag_id,first_name,last_name,dob,family_id,person_type_id,medical_problems,ssn,shelter_id,location_description,resume)
+VALUES ('12343', 'gene', 'reese', '1944-12-31', 1, 2, 'Lots again', '123-12-1234', 2, '??????','Linux, Windows');
+INSERT INTO people (tag_id,first_name,last_name,dob,family_id,person_type_id,medical_problems,ssn,shelter_id,location_description,resume)
+VALUES ('12345', 'Cory', 'Wright', '1900-03-11', 1, 3, 'Lots more', '123-12-1234', 3, 'He does not know','DNS, Subversion, PHP, Rails, Ruby');

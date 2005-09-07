131ab5b8faf2551574f6c9b95c67b1f2155b719a
Will Reese <wreese@gmail.com>
2005-09-07 23:43:24 +0000

    Added some events for people to the test data.

--- db/data.events.sql
+++ db/data.events.sql
@@ -1,2 +1,4 @@
 insert into events (event_time,event_type_id,volunteer_id) values ('2005-09-07 15:34:32',3,1);
 insert into events (event_time,event_type_id,volunteer_id) values ('2005-09-07 16:32:15',4,1);
+insert into events (event_time,event_type_id,person_id) values ('2005-09-07 15:34:32',3,1);
+insert into events (event_time,event_type_id,person_id) values ('2005-09-07 16:32:15',4,1);

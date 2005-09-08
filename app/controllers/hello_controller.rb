be6c156543ba737ceb5c94536af45c3dbe11dbd0
Cory Wright <corywright@gmail.com>
2005-09-08 11:00:25 +0000

    Added shelter name to totals list

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -4,5 +4,6 @@ class HelloController < ApplicationController
                                     "p.family_id=f.id and p.shelter_id=" + session[:shelter_id].to_s)
     @people = Person.count(["shelter_id = ?", session[:shelter_id] ])
     @volunteers = Volunteer.count(["shelter_id = ?", session[:shelter_id] ])
+    @shelter = Shelter.find(session[:shelter_id])
   end
 end

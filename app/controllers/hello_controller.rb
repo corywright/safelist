60f6dd1abeb2914622938c43792614915c4d515b
Cory Wright <corywright@gmail.com>
2005-09-10 23:08:44 +0000

    Correct counting of families

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,10 +1,9 @@
 class HelloController < ApplicationController
   def index
-    @families = Family.count_by_sql("select count(*) from people p, families f where " +
-                                    "p.family_id=f.id and p.shelter_id=" + session[:shelter_id].to_s)
+    @families = Family.count_by_sql("select count(distinct f.id) from families f join people p on(f.id=p.family_id) " +
+                                    "where p.shelter_id=" + session[:shelter_id].to_s)
     @people = Person.count(["shelter_id = ?", session[:shelter_id] ])
     @volunteers = Volunteer.count(["shelter_id = ?", session[:shelter_id] ])
     @shelter = Shelter.find(session[:shelter_id])
   end
-
 end

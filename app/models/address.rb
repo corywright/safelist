eef4a8c5f6a7c659dbb1f498a751f6e2ac4a3d48
Cory Wright <corywright@gmail.com>
2005-09-12 18:58:57 +0000

    Added copyright

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
+#
+# This file is part of the SafeList shelter management software.
+# Copyright 2005, Rackspace Managed Hosting
+#
+
 class Address < ActiveRecord::Base
     belongs_to :shelters
     belongs_to :people

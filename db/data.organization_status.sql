87b17b77f0106d9b9b5adda839342ee41352aee4
Brandon Williams <driftx@gmail.com>
2005-09-13 20:09:38 +0000

    Added active, inactive

--- db/data.organization_status.sql
+++ db/data.organization_status.sql
@@ -1,3 +1,5 @@
 insert into organization_status (name) values ('Pending');
 insert into organization_status (name) values ('Approved');
 insert into organization_status (name) values ('Rejected');
+insert into organization_status (name) values ('Active');
+insert into organization_status (name) values ('Inactive');

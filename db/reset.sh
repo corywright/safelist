d5b67ba05b34241dbda040138c45ce18da563fbd
Will Reese <wreese@gmail.com>
2005-09-13 05:51:53 +0000

    Modified the reset.sh script to use cwright as the user to match database.yml.

--- db/reset.sh
+++ db/reset.sh
@@ -5,20 +5,20 @@ dropdb safelist_test
 sleep 1;
 createdb safelist_development
 createdb safelist_test
-psql safelist_development <create.sql
-psql safelist_development < data.person_types.sql
-psql safelist_development < data.addresses.sql
-psql safelist_development < data.shelters.sql
-psql safelist_development < data.event_types.sql
-psql safelist_development < data.family0.sql
+psql -U cwright safelist_development <create.sql
+psql -U cwright safelist_development < data.person_types.sql
+psql -U cwright safelist_development < data.addresses.sql
+psql -U cwright safelist_development < data.shelters.sql
+psql -U cwright safelist_development < data.event_types.sql
+psql -U cwright safelist_development < data.family0.sql
 
 # test data
-psql safelist_development < data.families.sql
-psql safelist_development < data.people.sql
-psql safelist_development < data.volunteers.sql
-psql safelist_development < data.events.sql
-psql safelist_development < data.injury_reports.sql
-psql safelist_development < data.organization_types.sql
-psql safelist_development < data.organization_statuses.sql
+psql -U cwright safelist_development < data.families.sql
+psql -U cwright safelist_development < data.people.sql
+psql -U cwright safelist_development < data.volunteers.sql
+psql -U cwright safelist_development < data.events.sql
+psql -U cwright safelist_development < data.injury_reports.sql
+psql -U cwright safelist_development < data.organization_types.sql
+psql -U cwright safelist_development < data.organization_statuses.sql
 
-psql safelist_test < create.sql
+psql -U cwright safelist_test < create.sql

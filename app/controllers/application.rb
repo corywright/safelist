9b696f13949fedb4a96536dce6a4c1f015bea000
Brandon Williams <driftx@gmail.com>
2005-09-13 21:06:48 +0000

    Initial support for grabbing http auth username for shelter_id

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -17,7 +17,8 @@ class ApplicationController < ActionController::Base
 
 	def get_shelter
 	  if !session[:shelter_id]
-	    @shelter = Shelter.find_by_domain(@request.host)
+		@user = get_auth_name
+	    @shelter = Shelter.find_by_domain(@user)
 	    if @shelter
 	      session[:shelter_id] = @shelter.id
 	    else
@@ -37,6 +38,27 @@ class ApplicationController < ActionController::Base
 	  return [pages, slice]
 	end
 
+    def get_auth_name
+      user = '' 
+      # extract authorisation credentials 
+      if request.env.has_key? 'X-HTTP_AUTHORIZATION' 
+        # try to get it where mod_rewrite might have put it 
+        authdata = request.env['X-HTTP_AUTHORIZATION'].to_s.split 
+      elsif request.env.has_key? 'Authorization' 
+        # for Apace/mod_fastcgi with -pass-header Authorization 
+        authdata = request.env['Authorization'].to_s.split 
+      elsif request.env.has_key? 'HTTP_AUTHORIZATION' 
+        # this is the regular location 
+        authdata = request.env['HTTP_AUTHORIZATION'].to_s.split  
+      end 
+
+      # at the moment we only support basic authentication 
+      if authdata and authdata[0] == 'Basic' 
+        user, pass = Base64.decode64(authdata[1]).split(':')[0..1] 
+      end 
+      return user
+  end
+	
 	$CITIZEN_CHECKIN = 1
 	$CITIZEN_CHECKOUT = 2
 	$VOLUNTEER_CHECKIN = 3

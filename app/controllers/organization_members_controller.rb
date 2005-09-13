d0499d1d64a7fa2305f7a2f0ccb6189bf435eb17
Brandon Williams <driftx@gmail.com>
2005-09-13 03:30:58 +0000

    Members can checkin/out, in theory, no db to test with yet.

--- app/controllers/organization_members_controller.rb
+++ app/controllers/organization_members_controller.rb
@@ -25,6 +25,7 @@ class OrganizationMembersController < ApplicationController
   def create
     @organization_member = OrganizationMember.new(params[:organization_member])
     @organization_member.organization_id = session[:last_organization]
+    @organization_member.department_id = @params[:department_id]
     if @organization_member.save
       flash[:notice] = 'OrganizationMember was successfully created.'
       if session[:history][2]['name'] == 'organizations:show'
@@ -52,8 +53,28 @@ class OrganizationMembersController < ApplicationController
     end
   end
 
-  def destroy
-    OrganizationMember.find(params[:id]).destroy
-    redirect_to :action => 'list'
+#  def destroy
+#    OrganizationMember.find(params[:id]).destroy
+#    redirect_to :action => 'list'
+#  end
+  def checkinout
+    @organization_member = OrganizationMember.find(params[:id])
+
+    @event = Event.new
+    @event.shelter_id = session[:shelter_id]
+    if @organization_member.checked_in
+      @event.event_type = EventType.find(11)
+    else
+      @event.event_type = EventType.find(10)
+    end
+    @event.organization_member_id = @organization_member.id
+    @event.event_time = Time.now
+    if @event.save
+      flash[:notice] = "#{@event.event_type.name} successful"
+      redirect_to :action => 'list'
+    else
+      flash[:error] = "#{@event.event_type.name} failed"
+    end
   end
+
 end
--- app/models/organization_member.rb
+++ app/models/organization_member.rb
@@ -6,4 +6,29 @@
 class OrganizationMember < ActiveRecord::Base
 	belongs_to :organization
 	belongs_to :department
+
+  def last_check_in
+      @event = Event.find_by_organization_member_id_and_event_type_id(self.id, 10, :order => "event_time DESC")
+      write_attribute(:last_check_in, @event.event_time) if @event
+  end
+  def last_check_out
+      @event = Event.find_by_organization_member_id_and_event_type_id(self.id, 11, :order => "event_time DESC")
+      write_attribute(:last_check_out, @event.event_time) if @event
+  end
+  def last_event
+      Event.find_by_organization_member_id(self.id, :order => "event_time DESC");
+  end
+  def checked_in
+    if self.last_check_in
+      if (self.last_check_in.to_i > self.last_check_out.to_i)
+        write_attribute(:checked_in, true)
+      else
+        write_attribute(:checked_in, false)
+      end
+    else
+        # never logged in
+        write_attribute(:checked_in, false)
+    end
+  end
+
 end

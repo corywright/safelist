508018357ba02259f477173f976d9708c3b9ad2c
Brandon Williams <driftx@gmail.com>
2005-09-13 22:38:58 +0000

    Removed sha digest on passwd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,7 @@
-require 'digest/sha1' 
-
 class User < ActiveRecord::Base 
-  def passwd=(str) 
-    write_attribute("passwd", Digest::SHA1.hexdigest(str)) 
-  end 
-
-  def passwd 
-    "*****"  
-  end 
-
   def self.authenticate(username, passwd) 
-	if passwd.nil?
-		passwd = ''
-	end
     find(:first, :conditions => [ "username = ? AND passwd =?", 
                username, 
-               Digest::SHA1.hexdigest(passwd) ]) 
+               passwd]) 
   end 
 end

36fe2643cd769b3c758bd3bd5caf74f0d3597b97
Brandon Williams <driftx@gmail.com>
2005-09-13 19:31:21 +0000

    Combined status/approval status stuff

--- db/data.event_types.sql
+++ db/data.event_types.sql
@@ -12,3 +12,5 @@ insert into event_types (name) values ('Organization Member Check Out');
 insert into event_types (name) values ('Organization Pending');
 insert into event_types (name) values ('Organization Approval');
 insert into event_types (name) values ('Organization Rejected');
+insert into event_types (name) values ('Organization Activated');
+insert into event_types (name) values ('Organization Deactivated');

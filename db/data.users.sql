65fa10d82ae376ff5118fbd63d04c9b5882a0cfe
Brandon Williams <driftx@gmail.com>
2005-09-14 05:02:27 +0000

    Default users

---
+++ db/data.users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (login, password) VALUES ('windsor', 'windsor');
+INSERT INTO users (login, password) VALUES ('levi', 'levi');
+INSERT INTO users (login, password) VALUES ('freeman', 'freeman');
+INSERT INTO users (login, password) VALUES ('rackspace', 'rackspace');
+INSERT INTO users (login, password) VALUES ('kelly171', 'kelly171');
+INSERT INTO users (login, password) VALUES ('kelly1536', 'kelly1536');
--- db/reset.sh
+++ db/reset.sh
@@ -20,5 +20,6 @@ psql -U cwright safelist_development < data.events.sql
 psql -U cwright safelist_development < data.injury_reports.sql
 psql -U cwright safelist_development < data.organization_types.sql
 psql -U cwright safelist_development < data.organization_status.sql
+psql -U cwright safelist_development < data.users.sql
 
 psql -U cwright safelist_test < create.sql

2d98b8540ef743951d99195fd8be13d47e844f68
Brett Elmendorf <brett@r26d.com>
2005-09-16 05:28:32 +0000

    removed shelter

--- db/data.shelters.sql
+++ db/data.shelters.sql
@@ -1,5 +1,5 @@
 insert into shelters (name,address_id,tag_id_prefix,domain) values('Unknown',1,'U','*');
-insert into shelters (name,address_id,tag_id_prefix,domain) values('Windsor World Shelter',2,'W','windsor.sasafelist.org');
+insert into shelters (name,address_id,tag_id_prefix,domain) values('Windsor World',2,'W','windsor.sasafelist.org');
 insert into shelters (name,address_id,tag_id_prefix,domain) values('Levi Strauss',3,'L','levi.sasafelist.org');
 insert into shelters (name,address_id,tag_id_prefix,domain) values('Kelly USA',4,'K','kelly.sasafelist.org');
 insert into shelters (name,address_id,tag_id_prefix,domain) values('XXXXX',1,'X','X.sasafelist.org');

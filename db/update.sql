040ecea0dddf60ffc13ed90555cb5b6e29aabf58
Will Reese <wreese@gmail.com>
2005-09-18 17:56:24 +0000

    Cleaned up the update script to get ready for production update.

--- db/update.sql
+++ db/update.sql
@@ -1,25 +1,20 @@
 begin;
 
-insert into event_types (name) values ('Organization Pending');
-insert into event_types (name) values ('Organization Approval');
-insert into event_types (name) values ('Organization Rejected');
-insert into event_types (name) values ('Organization Activated');
-insert into event_types (name) values ('Organization Deactivated');
-
 create table badge_status (
     id serial primary key,
     name text not null default ''
 );
 create index badge_status_name on badge_status (name);
 
+insert into badge_status (id,name) values (1,'Not Started');
+insert into badge_status (id,name) values (2,'Badge Printed');
+insert into badge_status (id,name) values (3,'Badge Issued');
+select setval('badge_status_id_seq',3);
+
 alter table people add column badge_status_id references badge_status (id);
 alter table people alter column badge_status_id set default 1;
 update people set badge_status_id = 1;
 alter table people alter column badge_status_id set not null;
-
-insert into badge_status (name) values ('Not Started');
-insert into badge_status (name) values ('Badge Printed');
-insert into badge_status (name) values ('Badge Issued');
 
 rollback;
 --commit;

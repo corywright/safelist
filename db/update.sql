7ddb20d19dd33c5e2859820e18d1b855c86cdbff
Will Reese <wreese@gmail.com>
2005-09-18 17:59:56 +0000

    Removed explicit commit and rollback commands and added missing column type.

--- db/update.sql
+++ db/update.sql
@@ -1,5 +1,3 @@
-begin;
-
 create table badge_status (
     id serial primary key,
     name text not null default ''
@@ -11,10 +9,7 @@ insert into badge_status (id,name) values (2,'Badge Printed');
 insert into badge_status (id,name) values (3,'Badge Issued');
 select setval('badge_status_id_seq',3);
 
-alter table people add column badge_status_id references badge_status (id);
+alter table people add column badge_status_id integer references badge_status (id);
 alter table people alter column badge_status_id set default 1;
 update people set badge_status_id = 1;
 alter table people alter column badge_status_id set not null;
-
-rollback;
---commit;

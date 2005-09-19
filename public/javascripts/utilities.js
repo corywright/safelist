40d4035344c105d6a6033a1b7c33a8a7ce315c46
Cory Wright <corywright@gmail.com>
2005-09-19 21:22:10 +0000

    Moved the javascript code out into its own file.  Add any other Javascript utility functions to public/javascripts/utilities.js

---
+++ public/javascripts/utilities.js
@@ -0,0 +1,30 @@
+
+
+// Code used on the views/families/_peopleform.rhtml page
+
+function autoTab(input,len, e) {
+    var isNN = (navigator.appName.indexOf("Netscape")!=-1);
+    var keyCode = (isNN) ? e.which : e.keyCode; 
+    var filter = (isNN) ? [0,8,9] : [0,8,9,16,17,18,37,38,39,40,46];
+    if(input.value.length >= len && !containsElement(filter,keyCode)) {
+	input.value = input.value.slice(0, len);
+	input.form[(getIndex(input)+1) % input.form.length].focus();
+    }
+    function containsElement(arr, ele) {
+	var found = false, index = 0;
+	while(!found && index < arr.length)
+	    if(arr[index] == ele)
+		found = true;
+	    else
+		index++;
+	return found;
+    }
+    function getIndex(input) {
+	var index = -1, i = 0, found = false;
+	while (i < input.form.length && index == -1)
+	    if (input.form[i] == input)index = i;
+	    else i++;
+	return index;
+    }
+    return true;
+}

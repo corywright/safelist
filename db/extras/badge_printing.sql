268a813e3483232eb9d235e9d5ae077c402960e6
Will Reese <wreese@gmail.com>
2005-09-19 04:01:42 +0000

    Excluded family 0 from the concatenate family members function.

--- db/extras/badge_printing.sql
+++ db/extras/badge_printing.sql
@@ -6,14 +6,18 @@ declare
   family_members text := '''';
   member record;
 begin
-  for member in select p.first_name || '' '' || p.last_name as full_name, case when p.tag_id = '''' then s.tag_id_prefix || p.id else p.tag_id end as id_number from people p join shelters s on (p.shelter_id = s.id) where p.family_id = my_id loop
-    family_members := family_members || ''\n'' || member.full_name || '' / '' || member.id_number;
-  end loop;
-  return trim(both ''\n'' from family_members);
+  if my_id <> 0 then
+    for member in select p.first_name || '' '' || p.last_name as full_name, case when p.tag_id = '''' then s.tag_id_prefix || p.id else p.tag_id end as id_number from people p join shelters s on (p.shelter_id = s.id) where p.family_id = my_id loop
+      family_members := family_members || ''\n'' || member.full_name || '' / '' || member.id_number;
+    end loop;
+    return trim(both ''\n'' from family_members);
+  else
+    return family_members;
+  end if;
 end;
 ' language plpgsql;
 
---drop view exporter_badge_info;
+drop view exporter_badge_info;
 create view exporter_badge_info as select first_name || ' ' || last_name as full_name, p.id, case when p.tag_id ='' then s.tag_id_prefix || p.id else p.tag_id end, s.name as shelter, decode(pic.image,'base64') as image, concat_family_members(p.family_id) as family_members from people p join shelters s on (p.shelter_id = s.id) join pictures pic on (pic.person_id = p.id) where p.badge_status_id = '1' order by pic.id limit 20;
 grant select on exporter_badge_info to exporter;
 grant select on people to exporter;
